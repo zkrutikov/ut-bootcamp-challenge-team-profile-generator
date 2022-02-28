a65e1aa15441e1c7ec130051bddee0e2f5b628bd
Zak Krutikov <zak.krutikov@gmail.com>
2022-02-27 23:27:26 -0500

    tweaked generateHTMl

--- generateHTML.js
+++ generateHTML.js
@@ -1,89 +1,137 @@
-const Employee = require("./lib/Employee");
-const fs = require ('fs');
-
-const generateTeam = function (team) {
-    const generateManager = function (manager) {
-     return `
-     <div>
-     <p>${manager.getName()}</p>
-     <p>${manager.getId()}</p>
-     <p>${manager.getEmail()}</p>
-     <p>${manager.getOfficeNumber()}</p>
-     </div>
-     `
-    }
-    const generateEngineer = function (engineer) {
-        return `
-        <div>
-        <p>${engineer.getName()}</p>
-        <p>${engineer.getId()}</p>
-        <p>${engineer.getEmail()}</p>
-        <p>${engineer.getGithub()}</p>
-        </div>
-        `
-       }
-
-    const generateIntern = function (intern) {
-        return `
-        <div>
-        <p>${intern.getName()}</p>
-        <p>${intern.getId()}</p>
-        <p>${intern.getEmail()}</p>
-        <p>${intern.getSchool()}</p>
-        </div>
-        `
-       }
-
-    const html = [];
-    html.push(team.filter(employee => employee.getRole() == 'Manager')
-    .map(manager => generateManager(manager))
-    );
-    html.push(team.filter(employee => employee.getRole() == 'Engineer')
-    .map(engineer => generateEngineer(engineer))
-    );
-    html.push(team.filter(employee => employee.getRole() == 'Intern')
-    .map(intern => generateIntern(intern))
-    );
-    return html.join('');
-}
-
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
-
-const generateHTML = function(team) {
+const generateManager = function (manager) {
     return `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8" />
-   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-   <title>My Team</title>
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   <link rel="stylesheet" href="style.css">
-   <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
-<body>
-   <div class="container-fluid">
-       <div class="row">
-           <div class="col-12 jumbotron mb-3 team-heading">
-               <h1 class="text-center">My Team</h1>
-           </div>
-       </div>
-   </div>
-   <div class="container">
-       <div class="row">
-           <div class="team-area col-12 d-flex justify-content-center">
-               ${generateTeam(team)}
-           </div>
-       </div>
-   </div>
-</body>
-</html>
-    `
+    <div class="card m-auto">
+    <header class="card-header-title-is-centered">
+    <p class="has-text-centered is-size-3">
+    ${manager.name}</p>
+    <p class="has-text-centered is-size-4">Manager</p>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <p class="id">ID: ${manager.id}</p>
+        <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+        <p class="office">Office Number: ${manager.officeNumber}</p>
+      </div>
+    </div>
+  </div>
+    `;
+
 }
 
+//Create Engineer card
+const generateEngineer = function (engineer) {
+    return `
+    <div class="card m-auto">
+    <header class="card-header-title-is-centered">
+    <p class="has-text-centered is-size-3">
+    ${engineer.name}</p>
+    <p class="has-text-centered is-size-4">Engineer</p>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <p class="id">ID: ${engineer.id}</p>
+        <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+        <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+      </div>
+    </div>
+  </div>
+    `;
+}
+//Create intern card
+const generateIntern = function (intern) {
+    return `
+ 
+    <div class="card m-auto">
+    <header class="card-header-title-is-centered">
+    <p class="has-text-centered is-size-3">
+    ${intern.name}</p>
+    <p class="has-text-centered is-size-4">Intern</p>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <p class="id">ID: ${intern.id}</p>
+        <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+        <p class="school">School: ${intern.school}</p>
+      </div>
+    </div>
+  </div>
+    `;
+}
 
-module.exports = generateHTML;
+//Push array to page 
+generateHTML = (data) => {
+
+//Array for cards 
+    pageArray = []; 
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole(); 
+
+
+//Call manager function
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+
+            pageArray.push(managerCard);
+        }
+
+//Call engineer function
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+
+            pageArray.push(engineerCard);
+        }
+
+//Call intern function 
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+
+            pageArray.push(internCard);
+        }
+        
+    }
+
+//Joining strings 
+    const employeeCards = pageArray.join('')
+//Return to generated page
+    const generateTeam = generateTeamPage(employeeCards); 
+    return generateTeam;
+
+}
+
+// generate html page 
+const generateTeamPage = function (employeeCards) {   
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header class="has-text-centered m-auto">
+          <nav class =' is-centered is-vcentered is-mobile'>
+                <span class="is-size-1 has-text-centered has-text-weight-bold">Team Profile</span>
+            </nav>
+        </header>
+        <main>
+            <div class="column">
+                <div class="row justify-content-center columns" id="team-cards">
+                    <!--Team Cards-->
+                    ${employeeCards}
+                </div>
+            </div>
+        </main>
+        
+    </body>
+  
+    </html>
+  `;
+  }
+  
+  // export to index
+  module.exports = generateHTML; 
